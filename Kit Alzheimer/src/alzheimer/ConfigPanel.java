cbd4fc37e95e49038c446d4564d2f7eabe5d8a77
Alex Gonzalez <Alex@192.168.0.195>
2014-07-01 23:24:18 +0200

    kit alzheimer

--- Kit Alzheimer/src/alzheimer/ConfigPanel.java
+++ Kit Alzheimer/src/alzheimer/ConfigPanel.java
@@ -20,7 +20,7 @@ public class ConfigPanel extends JFrame {
 	public static Config_basico basico;
 
 	/**
-	 * Launch the application. funciona
+	 * Launch the application. funciona1
 	 * 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
