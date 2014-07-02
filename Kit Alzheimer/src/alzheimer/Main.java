1db566b8d31dd872f674242c7738853f54e3da8f
Alex Gonzalez <Alex@Alex-PC>
2014-07-02 09:28:08 +0200

    Main

---
+++ Kit Alzheimer/src/alzheimer/Main.java
@@ -0,0 +1,75 @@
+package alzheimer;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+	public static Configuracion config;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		try {
+			JFrame.setDefaultLookAndFeelDecorated(true);
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 544, 453);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		config = new Configuracion();
+		JPanel panel = config;
+		panel.setBounds(10, 40, 274, 343);
+		contentPane.add(panel);
+				
+		
+		
+		JLabel lblConfig = new JLabel("Config");
+		lblConfig.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		lblConfig.setBounds(10, 11, 261, 27);
+		contentPane.add(lblConfig);
+	}
+}
